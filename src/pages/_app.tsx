069b80078125e5c1ffe977b668a10f74e3a61c5b
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-02-27 02:40:00 -0300

    add use of cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 
 import React from 'react';
 import { ChallengesProvider } from '../contexts/ChallengesContext';
-import { CountdownProvider } from '../contexts/CountdownContext';
 import '../styles/global.css'
 
-function MyApp({ Component, pageProps }) { //componente que está em todas as páginas
+function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
