b50a0858485e2870b0e5010ed5b19c2ec43df4df
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-03-02 16:55:50 -0300

    add request data in profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,14 +2,15 @@ import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
-export function Profile() {
+
+export function Profile({ githubName, githubImageUrl }) {
   const { level } = useContext(ChallengesContext);
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/ruangoa.png" alt="Ruan Gomes" />
+      <img src={githubImageUrl} alt="Ruan Gomes" />
       <div>
-        <strong>Ruan Gomes</strong>
+        <strong>{githubName}</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Level {level}
