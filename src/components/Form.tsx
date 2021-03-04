415dc03921e6ee623bc09d7a38b62dda817321ea
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-03-04 09:43:54 -0300

    add onkeydown in submit github handle input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,38 +6,59 @@ import { useRouter } from 'next/router';
 import { getDataFromGithub } from '../services/github';
 import styles from '../styles/components/Form.module.css';
 
-export function Form() {
+export function Form({ children }) {
   const [githubHandle, setGithubHandle] = useState("");
   const history = useRouter();
 
   const validateGithubHandle = async () => {
-    const data = await getDataFromGithub(githubHandle)
-      .then((data) => data)
-      .catch((err) => { alert(err) });
+    if (githubHandle.length > 0) {
+      const data = await getDataFromGithub(githubHandle)
+        .then((data) => data)
+        .catch((err) => { alert(err) });
 
-    if (data["name"]) {
-      Cookies.set('githubName', String(data["name"]));
-      Cookies.set('githubImageUrl', String(data["avatar_url"]));
-      history.push("/home");
-    } else {
-      alert("Github handle does not exist");
+      if (data["name"]) {
+        const cookiesNames = ["currentExperience", "level", "githubImageUrl", "challengesCompleted", "githubName"];
+        cookiesNames.map((name) => { Cookies.remove(name) });
+        Cookies.remove('currentExperience')
+        Cookies.set('githubName', String(data["name"]));
+        Cookies.set('githubImageUrl', String(data["avatar_url"]));
+        history.push("/home");
+      } else {
+        alert("Github handle does not exist");
+      }
+      setGithubHandle("");
     }
-    setGithubHandle("");
   }
 
   const handleChange = async (event) => {
     setGithubHandle(event.target.value);
   }
 
+  const handleEnterKey = (e) => {
+    if (e.keyCode === 13) {
+      validateGithubHandle();
+    }
+  }
+
   return (
     <div className={styles.form}>
-      <div className={styles.title}>
-        <img src="/logo-full.svg" alt="Fechar modal" />
-      </div>
+      {children}
       <div className={styles.input}>
         <p>Github handle</p>
-        <input type="text" value={githubHandle} onChange={handleChange} name="githubName" className={styles.inputBox} />
-        <input type="submit" value="Submit" onClick={validateGithubHandle} className={styles.inputButton} />
+        <input
+          type="text"
+          value={githubHandle}
+          onChange={handleChange}
+          name="githubName"
+          className={styles.inputBox}
+          onKeyDown={handleEnterKey}
+        />
+        <input
+          type="submit"
+          value="Submit"
+          onClick={validateGithubHandle}
+          className={styles.inputButton}
+        />
       </div>
     </div>
   )
